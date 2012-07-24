1d7acfac77ff4a8eb6b9fbd69df32b648ea8f708
Krzysztof Kosyl <krzysztof.kosyl@gmail.com>
2012-07-25 00:25:20 +0200

    Parametrization - vocab_size

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,39 +1,35 @@
-var NewVocab_lex = [
-	[/s$/, ''],
-	[/es$/, ''],
-	[/es$/, 'e'],
-	[/ies$/, 'y'],
-];
 
-var NewVocab_dict = {};
-for (var i = 0; i < NewVocab_data.length; i++) {
-	NewVocab_dict[NewVocab_data[i]] = true;
-}
+function textNodeIterator(node, func) {
+	'use strict';
 
-function textNodeIterator(node, func){
-	if(node.nodeType === 3){
-		func(node)
-	} else if(node.nodeType === 1) {
-		var children = node.childNodes;
-		for(var i = 0; i < children.length; i++) {
+	if (node.nodeType === 3) {
+		func(node);
+	} else if (node.nodeType === 1) {
+		var children = node.childNodes, i;
+		for (i = 0; i < children.length; i += 1) {
 			textNodeIterator(children[i], func);
 		}
 	}
 }
 
-function isInWords(word) {
-	return NewVocab_dict[word] === true;
+function isInWords(config, word) {
+	'use strict';
+
+	return config.dict[word] === true;
 }
 
-function isNew(word) {
-	if (isInWords(word)) {
+function isNew(config, word) {
+	'use strict';
+
+	if (isInWords(config, word)) {
 		return false;
 	}
-	
-	for (var i = 0; i < NewVocab_lex.length; i++) {
-		var lex = NewVocab_lex[i];
-		var base_word = word.replace(lex[0], lex[1]);
-		if (isInWords(base_word)) {
+
+	var i = 0;
+	for (i = 0; i < config.lex.length; i += 1) {
+		var lex = config.lex[i],
+			base_word = word.replace(lex[0], lex[1]);
+		if (isInWords(config, base_word)) {
 			return false;
 		}
 	}
@@ -41,14 +37,18 @@ function isNew(word) {
 	return true;
 }
 
-function hlText(content) {
-	var result = [];
-	var parts = content.split(/([a-zA-Z]+)/)
-	
-	for(var i = 0; i < parts.length; i++) {
+function highlightText(config, content) {
+	'use strict';
+
+	var result = [],
+		parts = content.split(/([a-zA-Z']+)/),
+		i = 0;
+		part = null;
+
+	for (i = 0; i < parts.length; i += 1) {
 		var part = parts[i];
-		if (i % 2 == 1) {
-			if (isNew(part.toLowerCase())) {
+		if (i % 2 === 1) {
+			if (isNew(config, part.toLowerCase())) {
 				result.push('<span style="background-color: #ffff00; color: #0000ff;">' + part + '</span>');
 			} else {
 				result.push(part);
@@ -60,12 +60,33 @@ function hlText(content) {
 	return result.join('');
 }
 
-textNodeIterator(document.body, function (node) {
-	var newNode = document.createElement("span");
-	var parentNode = node.parentNode;
+function highlightPage(vocab_size) {
+	'use strict';
 
-	newNode.innerHTML = hlText(node.data);
-	parentNode.replaceChild(newNode, node);
-	
-	return 0;
-})
+	var dict = {}, 
+		i = 0;
+	for (i = 0; i < Math.min(NewVocab_data.length, vocab_size); i += 1) {
+		dict[NewVocab_data[i]] = true;
+	}
+
+	var config = {
+		'vocab_size': vocab_size,
+		'dict': dict,
+		'lex': [
+			[/s$/, ''],
+			[/es$/, ''],
+			[/es$/, 'e'],
+			[/ies$/, 'y']
+		],
+	};
+
+	textNodeIterator(document.body, function (node) {
+		var newNode = document.createElement("span"),
+			parentNode = node.parentNode;
+
+		newNode.innerHTML = highlightText(config, node.data);
+		parentNode.replaceChild(newNode, node);
+
+		return 0;
+	});
+}
